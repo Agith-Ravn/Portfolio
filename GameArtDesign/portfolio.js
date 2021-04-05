7d36bcb479c24a54f777941a6a34a4514e066851
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-04-05 20:37:45 +0200

    .

--- GameArtDesign/portfolio.js
+++ GameArtDesign/portfolio.js
@@ -13,9 +13,11 @@ window.onscroll = function() {
 
 /* Slideshow */
 var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"]
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
+
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
